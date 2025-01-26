54b983552ed6bc8c0090c8de80bcd75079625e0e
Om Rindhe <rindheom43.com>
2025-01-26 23:59:55 +0530

    day1

---
+++ Day1/script.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+//  file creation
+// fs.writeFile("msg.txt","welcome to backend series.",function(err){
+
+//     if(err) console.log(error);
+//     else console.log("file created");
+// })
+
+ /// update data
+
+// fs.appendFile("msg.text","this is append msg from om rindhe", function(err){
+//     if(err) console.log(err.message);
+//     else console.log("data updated");
+// });
+
+// fs.rename("msg2.text","msg.text",function(err){
+//     if(err) console.log(err);
+//     else console.log("file name change sucessfully")
+// })
+
+
+
+
